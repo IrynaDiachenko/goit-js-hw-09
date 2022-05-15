052d21139a6737adf3b6c767176c046d440fea8c
IrynaDiachenko <diachenko.iryna7@gmail.com>
2022-05-15 15:30:12 +0300

    Task 3 push one

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,39 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onSubmitClick);
+
+function onSubmitClick(event) {
+  event.preventDefault();
+  const { delay, step, amount } = event.currentTarget;
+
+  let dalayPromise = Number(delay.value);
+
+  for (let i = 1; i <= amount.value; i++) {
+    createPromise(i, dalayPromise).then(onFulled).catch(onRegected);
+    dalayPromise += Number(step.value);
   }
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, rejected) => {
+    const canFulfill = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (canFulfill) {
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      }
+
+      rejected(`❌ Rejected promise ${position} in ${delay}ms`);
+    }, delay);
+  });
+}
+
+function onFulled(result) {
+  Notify.success(result);
+}
+
+function onRegected(error) {
+  Notify.failure(error);
 }
