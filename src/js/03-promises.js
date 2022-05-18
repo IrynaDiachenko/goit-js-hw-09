f038df56276ca56ea3818c9d82720010371c057d
IrynaDiachenko <diachenko.iryna7@gmail.com>
2022-05-18 20:06:35 +0300

    Check last

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -11,13 +11,13 @@ function onSubmitClick(event) {
   let dalayPromise = Number(delay.value);
 
   for (let i = 1; i <= amount.value; i++) {
-    createPromise(i, dalayPromise).then(onFulled).catch(onRegected);
+    createPromise(i, dalayPromise).then(onFulfilled).catch(onRejected);
     dalayPromise += Number(step.value);
   }
 }
 
 function createPromise(position, delay) {
-  return new Promise((resolve, rejected) => {
+  return new Promise((resolve, reject) => {
     const canFulfill = Math.random() > 0.3;
 
     setTimeout(() => {
@@ -25,15 +25,15 @@ function createPromise(position, delay) {
         resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
       }
 
-      rejected(`❌ Rejected promise ${position} in ${delay}ms`);
+      reject(`❌ Rejected promise ${position} in ${delay}ms`);
     }, delay);
   });
 }
 
-function onFulled(result) {
+function onFulfilled(result) {
   Notify.success(result);
 }
 
-function onRegected(error) {
+function onRejected(error) {
   Notify.failure(error);
 }
